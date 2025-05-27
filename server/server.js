173c73e81d26e8abc7bc9d97cfaa240a65484935
Sarahkhan20 <sarahejaz77@gmail.com>
2025-05-27 13:17:13 +0530

    server.js changes for build

--- server/server.js
+++ server/server.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 5000;
 const path = require("path");
 
 // only when ready to deploy
-app.use(express.static("../client/build"));
+app.use(express.static("../frontend/build"));
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // only when ready to deploy
 app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
 );
 // Initialize Gemini AI
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
